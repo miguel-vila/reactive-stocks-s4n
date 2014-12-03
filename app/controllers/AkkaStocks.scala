c56a69539c1a1e2c9ff1d3133cdcee89c2440ec0
Miguel Vilá <miguelvilag@gmail.com>
2014-12-03 10:30:17 -0500

    Estado inicial pre-ejercicio 1

--- app/controllers/AkkaStocks.scala
+++ app/controllers/AkkaStocks.scala
@@ -1,28 +1,40 @@
 package controllers
 
-import backend.AverageManagerMock
 import model.StockAverage
 import play.api.mvc.{AnyContent, WebSocket, Action, Controller}
 import actors.UserActor
 import play.api.libs.json._
-
 import play.api.Play.current
 
+/**
+ * Controlador de Play para manejar Stocks
+ */
 object AkkaStocks extends Controller {
 
+  /**
+   * Esto es para poder serializar objetos de tipo StockAverage a JSON
+   */
   implicit val stockAverageFormat = Json.format[StockAverage]
 
+  /**
+   * Vista de index
+   */
   def index = Action {
-        Ok(views.html.index())
-    }
+    Ok(views.html.index())
+  }
 
-    def ws = WebSocket.acceptWithActor[JsValue, JsValue] { implicit request => out =>
-        UserActor.props(out)
-    }
+  /**
+   * Websocket
+   */
+  def ws = WebSocket.acceptWithActor[JsValue, JsValue] { implicit request => out =>
+    UserActor.props(out)
+  }
 
-    def averageStocks(): Action[AnyContent] = Action { req =>
-      val averages = AverageManagerMock.getStocksAverage()
-      Ok(Json.toJson(averages))
-    }
+  /**
+   * Devuelve el promedio de stocks
+   */
+  def averageStocks(): Action[AnyContent] = Action { req =>
+    ???
+  }
 
 }
--- app/model/Stock.scala
+++ app/model/Stock.scala
@@ -6,8 +6,10 @@ case class Stock(symbol: String) {
 
   val rand = Random
 
+  /**
+   * Retorna el valor promedio de este Stock
+   */
   def getAverageStock(): StockAverage = {
-    Thread.sleep(1000)
-    StockAverage(symbol, rand.nextDouble()*800)
+    ???
   }
 }
